abc440c7ce2b382540e13ee11c4e5713f8d2acb9
Max <test@mail.ru>
2022-12-08 16:49:38 +0300

    first commit this

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log(222);
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+let filmRate = +prompt('На сколько оцените его?', '');
+personalMovieDB.movies[lastFilm] = filmRate;
+
+lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+filmRate = +prompt('На сколько оцените его?', '');
+personalMovieDB.movies[lastFilm] = filmRate;
+
+console.log(personalMovieDB);
