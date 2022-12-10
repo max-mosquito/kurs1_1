66293c8f7228207e84380802cd0341d957665b4c
Max <test@mail.ru>
2022-12-10 12:24:04 +0300

    3-commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// location.reload()
+const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+if (!numberOfFilms) {
+  location.reload();
+}
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -8,12 +13,28 @@ const personalMovieDB = {
   privat: false,
 };
 
-let lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-let filmRate = +prompt('На сколько оцените его?', '');
-personalMovieDB.movies[lastFilm] = filmRate;
+if (personalMovieDB.count < 10) {
+  alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+  alert('Вы классический зритель');
+} else if (personalMovieDB.count > 30) {
+  alert('Вы киноман');
+} else {
+  alert('Произошла ошибка');
+  location.reload();
+}
 
-lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-filmRate = +prompt('На сколько оцените его?', '');
-personalMovieDB.movies[lastFilm] = filmRate;
+let lastFilm;
+let filmRate;
+for (let i = 0; i < 2; i++) {
+  lastFilm = prompt('Один из последних просмотренных фильмов?', '');
+  filmRate = +prompt('На сколько оцените его?', '');
+
+  if (lastFilm && filmRate && lastFilm.length < 10) {
+    personalMovieDB.movies[lastFilm] = filmRate;
+  } else {
+    i--;
+  }
+}
 
 console.log(personalMovieDB);
